2e603e1d320e735225fd7690cf52ed207c9f831c
mm2461 <43255719+mm2461@users.noreply.github.com>
2018-09-19 12:43:51 -0400

    Roll-a-Ball

---
+++ New Unity Project (2)/Assets/p.cs
@@ -0,0 +1,49 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class p : MonoBehaviour
+{
+    public Text countText;
+    public Text winText;
+    private Rigidbody rb;
+    public float speed;
+    private int count;
+
+
+    void Start()
+    {
+        float speed = 10;
+        rb = GetComponent<Rigidbody>();
+        count = 0;
+        SetCountText();
+        
+    }
+    void FixedUpdate()
+    {
+        float moveHorizontal = Input.GetAxis("Horizontal");
+        float moveVertical = Input.GetAxis("Vertical");
+
+        Vector3 movement = new Vector3(moveHorizontal, 0.0f, moveVertical);
+
+        rb.AddForce(movement * speed);
+    }
+    void OnTriggerEnter(Collider other)
+    {
+    if (other.gameObject.CompareTag("Pick up"))
+        {
+            other.gameObject.SetActive(false);
+            count = count + 1;
+            SetCountText();
+        }
+            }
+    void SetCountText()
+    { countText.text = "Count: " + count.ToString();
+    if (count >= 9)
+        {
+            winText.text = "You Win!";
+        }
+    }
+}
+
